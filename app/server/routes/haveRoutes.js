32d30105f91a464d930f1781704cba7240b33409
whereube <100147393+whereube@users.noreply.github.com>
2024-07-25 11:35:26 +0200

    get article done

--- app/server/routes/haveRoutes.js
+++ app/server/routes/haveRoutes.js
@@ -11,6 +11,179 @@ export const getHaveRoutes = () => {
     res.status(200).send(have);
   });
 
+  //function for finding matches based on singular 'article_id'. 
+  router.get('/getArticle', async (req, res, next) => {
+    const { article_id } = req.body;  
+
+    if (!article_id) {
+      return res.status(400).json({ message: 'Missing required article_id parameter' });
+    }
+
+    //SQL query that searches through 13 potential categories
+    try {
+      const [results, metadata] = await db.query(`
+      with matches as
+      (
+      select *, '1'  as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_1 = h.category_1 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id 
+      )
+      union
+      select *, '2' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_2 = h.category_2 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '3' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_3 = h.category_3 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '4' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_4 = h.category_4 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '5' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_5 = h.category_5 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '6' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_6 = h.category_6 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '7' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_7 = h.category_7
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '8' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_8 = h.category_8 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '9' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_9 = h.category_9 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '10' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_10 = h.category_10
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '11' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_11 = h.category_11 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      union
+      select *, '12' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_12 = h.category_12 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )    
+      union
+      select *, '13' as matchlevel
+      FROM wish w
+      where exists (
+        select * from have h
+        where w.category_13 = h.category_13 
+        and h.article_id = :article_id
+        and h.user_id != w.user_id
+      )
+      )
+      select * from matches
+      order by cast(matchlevel as int) desc`, {
+      replacements: { article_id },
+      type: db.QueryTypes.RAW
+    });
+
+    //Object created for sending API-response
+    let sortedMatches = {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+      10: [],
+      11: [],
+      12: [],
+      13: []
+    };
+
+    // Process results to populate sortedMatches
+    results.forEach(item => {
+      let { id, user_id, article_id, matchlevel } = item;
+      matchlevel = parseInt(matchlevel)
+      
+      sortedMatches[matchlevel].push({
+        id,
+        user_id,
+        article_id,
+        matchlevel
+      });  
+  });
+    console.log(sortedMatches);
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error fetching matches:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+  });
+
   //Function for finding users that wants what 'user_id' has. Matches are found based on a singular have request. 
   router.get('/matchForHave', async (req, res, next) => {
     const { user_id } = req.body;  
@@ -22,110 +195,127 @@ export const getHaveRoutes = () => {
     //SQL query that searches through 13 potential categories
     try {
       const [results, metadata] = await db.query(`
+      with matches as
+      (
       select *, '1'  as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_1 = h.category_1 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '2' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_2 = h.category_2 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '3' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_3 = h.category_3 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '4' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_4 = h.category_4 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '5' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_5 = h.category_5 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '6' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_6 = h.category_6 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '7' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_7 = h.category_7
-        and h.user_id = :user_id
-      )
-      union
-      select *, '8' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_8 = h.category_8 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '9' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_9 = h.category_9 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '10' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_10 = h.category_10
-        and h.user_id = :user_id
-      )
-      union
-      select *, '11' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_11 = h.category_11 
-        and h.user_id = :user_id
-      )
-      union
-      select *, '12' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_12 = h.category_12 
-        and h.user_id = :user_id
-      )    
-      union
-      select *, '13' as matchlevel
-      FROM wish w
-      where exists (
-        select * from have h
-        where w.category_13 = h.category_13 
-        and h.user_id = :user_id
-      )
-      order by matchlevel desc`, {
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_1 = h.category_1 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '2' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_2 = h.category_2 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '3' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_3 = h.category_3 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '4' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_4 = h.category_4 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '5' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_5 = h.category_5 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '6' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_6 = h.category_6 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '7' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_7 = h.category_7
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '8' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_8 = h.category_8 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '9' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_9 = h.category_9 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '10' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_10 = h.category_10
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '11' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_11 = h.category_11 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '12' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_12 = h.category_12 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )    
+        union
+        select *, '13' as matchlevel
+        FROM wish w
+        where exists (
+          select * from have h
+          where w.category_13 = h.category_13 
+          and h.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        )
+        select * from matches
+        order by cast(matchlevel as int) desc`, {
           replacements: { user_id },
           type: db.QueryTypes.RAW
       });
@@ -159,6 +349,7 @@ export const getHaveRoutes = () => {
           matchlevel
         });
       });
+      console.log(sortedMatches);
       res.status(200).json(results);
     } catch (error) {
       console.error('Error fetching wishes:', error);
--- app/server/routes/wishRoutes.js
+++ app/server/routes/wishRoutes.js
@@ -10,162 +10,6 @@ export const getWishRoutes = () => {
       });
       res.status(200).send(wish);
   });
-  
-  //function for finding matches based on singular 'article_id'. 
-  router.get('/getArticle', async (req, res, next) => {
-    const { article_id } = req.body;  
-
-    if (!article_id) {
-      return res.status(400).json({ message: 'Missing required article_id parameter' });
-    }
-
-    //SQL query that searches through 13 potential categories
-    try {
-      const [results, metadata] = await db.query(`
-        select *, '1'  as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_1 = h.category_1 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '2' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_2 = h.category_2 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '3' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_3 = h.category_3 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '4' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_4 = h.category_4 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '5' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_5 = h.category_5 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '6' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_6 = h.category_6 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '7' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_7 = h.category_7
-          and h.article_id = :article_id
-        )
-        union
-        select *, '8' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_8 = h.category_8 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '9' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_9 = h.category_9 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '10' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_10 = h.category_10
-          and h.article_id = :article_id
-        )
-        union
-        select *, '11' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_11 = h.category_11 
-          and h.article_id = :article_id
-        )
-        union
-        select *, '12' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_12 = h.category_12 
-          and h.article_id = :article_id
-        )    
-        union
-        select *, '13' as matchlevel
-        FROM wish w
-        where exists (
-          select * from have h
-          where w.category_13 = h.category_13 
-          and h.article_id = :article_id
-        )
-        order by matchlevel desc`, {
-          replacements: { article_id },
-          type: db.QueryTypes.RAW
-        });
-
-        //Object created for sending API-response
-        let sortedMatches = {
-          1: [],
-          2: [],
-          3: [],
-          4: [],
-          5: [],
-          6: [],
-          7: [],
-          8: [],
-          9: [],
-          10: [],
-          11: [],
-          12: [],
-          13: []
-        };
-
-        // Process results to populate sortedMatches
-        results.forEach(item => {
-          let { id, user_id, article_id, matchlevel } = item;
-          matchlevel = parseInt(matchlevel)
-          
-          sortedMatches[matchlevel].push({
-            id,
-            user_id,
-            article_id,
-            matchlevel
-          });  
-      });
-      console.log(sortedMatches);
-      res.status(200).json(results);
-    } catch (error) {
-      console.error('Error fetching matches:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
 
   //Function for finding users that have what 'user_id' is searching for. Matches are found based on a singular wish request. 
   router.get('/matchForWish', async (req, res, next) => {
@@ -178,110 +22,127 @@ export const getWishRoutes = () => {
     //SQL query that searches through 13 potential categories
     try {
       const [results, metadata] = await db.query(`
+      with matches as
+      (
       select *, '1'  as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_1 = w.category_1 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '2' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_2 = w.category_2 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '3' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_3 = w.category_3 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '4' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_4 = w.category_4 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '5' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_5 = w.category_5 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '6' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_6 = w.category_6 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '7' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_7 = w.category_7
-        and w.user_id = :user_id
-      )
-      union
-       select *, '8' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_8 = w.category_8 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '9' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_9 = w.category_9 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '10' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_10 = w.category_10
-        and w.user_id = :user_id
-      )
-      union
-       select *, '11' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_11 = w.category_11 
-        and w.user_id = :user_id
-      )
-      union
-       select *, '12' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_12 = w.category_12 
-        and w.user_id = :user_id
-      )    
-      union
-       select *, '13' as matchlevel
-      FROM have h
-      where exists (
-        select * from wish w
-        where h.category_13 = w.category_13 
-        and w.user_id = :user_id
-      )
-      order by matchlevel desc`, {
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_1 = w.category_1 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '2' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_2 = w.category_2 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '3' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_3 = w.category_3 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '4' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_4 = w.category_4 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '5' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_5 = w.category_5 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '6' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_6 = w.category_6 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '7' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_7 = w.category_7
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '8' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_8 = w.category_8 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '9' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_9 = w.category_9 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '10' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_10 = w.category_10
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '11' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_11 = w.category_11 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        union
+        select *, '12' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_12 = w.category_12 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )    
+        union
+        select *, '13' as matchlevel
+        FROM have h
+        where exists (
+          select * from wish w
+          where h.category_13 = w.category_13 
+          and w.user_id = :user_id
+          and h.user_id != w.user_id
+        )
+        )
+        select * from matches
+        order by cast(matchlevel as int) desc`, {
           replacements: { user_id },
           type: db.QueryTypes.RAW
       });
@@ -316,6 +177,7 @@ export const getWishRoutes = () => {
         });
           
       });
+      console.log(sortedMatches);
       res.status(200).json(results);
     } catch (error) {
       console.error('Error fetching matches:', error);
