4a4599800b95eda2734b029d2ed56b007f385d66
Esaiasson <teo.esaiasson@gmail.com>
2024-07-29 14:03:27 +0200

    added the function to the three part matching route

--- app/server/routes/multipartRoutes.js
+++ app/server/routes/multipartRoutes.js
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-import * as object from '../models/objectIndex.js';
-import { db } from '../database/databaseConnection.js';
 import validate from 'uuid-validate';
 import { generateSqlQuery, queryDb } from './requestFunctions.js'
 
@@ -22,7 +20,7 @@ export const getMultipartRoutes = () => {
       return res.status(400).json({ message: 'Invalid request format. The provided identifier must be a valid UUID.' });
     } 
 
-    const baseQuery = (i) => /*`
+    const baseQuery = (i) => `
         select w_p1.id as p1_id, w_p1.user_id p1_userid, w_p1.article_id p1_wish_articleid, h_p1.article_id p1_have_articleid, h_p2.id p2_id, h_p2.user_id p2_userid, w_p2.article_id p2_wish_article_id, h_p2.article_id p2_have_article_id, h_p3.id p3_id, h_p3.user_id p3_userid, w_p3.article_id p3_wish_article_id, h_p3.article_id p3_have_article_id, h_p4.id p4_id, h_p4.user_id p4_userid, w_p4.article_id p4_wish_article_id, h_p4.article_id p4_have_article_id, '${i}' as matchlevel  
         from wish w_p1
         inner join have h_p1 on w_p1.user_id = h_p1.user_id
@@ -39,25 +37,11 @@ export const getMultipartRoutes = () => {
         and w_p2.user_id != w_p3.user_id
         and w_p3.user_id != w_p4.user_id 
         AND w_p1.user_id = :user_id
-        `*/
-    	`
-            select w_p1.id as p1_id, w_p1.user_id p1_userid, w_p1.article_id p1_wish_articleid, h_p1.article_id p1_have_articleid, h_p2.id p2_id, h_p2.user_id p2_userid, w_p2.article_id p2_wish_article_id, h_p2.article_id p2_have_article_id, h_p3.id p3_id, h_p3.user_id p3_userid, w_p3.article_id p3_wish_article_id, h_p3.article_id p3_have_article_id, '${i}' as matchlevel  
-            from wish w_p1
-            inner join have h_p1 on w_p1.user_id = h_p1.user_id
-            inner join have h_p2 on w_p1.category_${i} = h_p2.category_${i} 
-            inner join wish w_p2 on h_p2.user_id = w_p2.user_id 
-            inner join have h_p3 on w_p2.category_${i} = h_p3.category_${i}
-            inner join wish w_p3 on h_p3.user_id = w_p3.user_id and w_p3.category_${i} = h_p1.category_${i}
-            where 
-            w_p1.user_id != w_p2.user_id
-            and w_p1.user_id != w_p3.user_id
-            and w_p2.user_id != w_p3.user_id 
-            AND w_p1.user_id = :user_id
         `
     const sqlQuery = generateSqlQuery(baseQuery, category_range)
     const replacementValues = { 'user_id': user_id }
 
-    const response = await queryDb(sqlQuery, replacementValues, objectConstruction)
+    const response = await queryDb(sqlQuery, replacementValues)
     
     if (response.accepted){
         res.status(200).json(response.results);
@@ -97,67 +81,14 @@ export const getMultipartRoutes = () => {
             AND w_p1.user_id = :user_id
         `
     const sqlQuery = generateSqlQuery(baseQuery, category_range)
+    const replacementValues = { 'user_id': user_id }
 
-    try {
-      const [results, metadata] = await db.query(
-        sqlQuery
-      , {
-          replacements: { user_id },
-          type: db.QueryTypes.RAW
-      });
-
-      //Object created for sending API-response
-      let sortedMatches = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
-        10: [],
-        11: [],
-        12: [],
-        13: []
-      };
-
-      // Process results to populate sortedMatches
-      results.forEach(item => {
-        let { 
-          p1_user_id,
-          p1_wish_article_id,
-          p1_have_article_id,
-          p2_user_id,
-          p2_wish_article_id,
-          p2_have_article_id,
-          p3_user_id,
-          p3_wish_article_id,
-          p3_have_article_id,
-          matchlevel
-        } = item;
-        matchlevel = parseInt(matchlevel)
-        
-        sortedMatches[matchlevel].push({
-          p1_user_id,
-          p1_wish_article_id,
-          p1_have_article_id,
-          p2_user_id,
-          p2_wish_article_id,
-          p2_have_article_id,
-          p3_user_id,
-          p3_wish_article_id,
-          p3_have_article_id,
-          matchlevel
-        });
-          
-      });
-      console.log(sortedMatches);
-      res.status(200).json(results);
-    } catch (error) {
-      console.error('Error fetching matches:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
+    const response = await queryDb(sqlQuery, replacementValues)
+    
+    if (response.accepted){
+        res.status(200).json(response.results);
+    } else{
+        res.status(500).json({ message: response.message });
     }
   });
 
