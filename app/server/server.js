4371bd2410204cc603edcaafb20ec25c45ace4a0
Esaiasson <teo.esaiasson@gmail.com>
2024-07-29 10:09:49 +0200

    added new route for multipart matching

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getWishRoutes } from './routes/wishRoutes.js';
 import { getHaveRoutes } from './routes/haveRoutes.js';
+import { getMultipartRoutes } from './routes/multipartRoutes.js';
 
 export const createServer = () => {
   const app = express();
@@ -11,6 +12,7 @@ export const createServer = () => {
   app.use(express.json());
   app.use('/wish', getWishRoutes());
   app.use('/have', getHaveRoutes());
+  app.use('/multipart', getMultipartRoutes());
 
   return app;
 };
